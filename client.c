081cbde430c6d849c723af11af7978fc2d6c3d25
OxyEho <valera808@inbox.ru>
2023-05-20 00:19:30 +0500

    client with delay

--- client.c
+++ client.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <time.h>
 
 #define CONF "config"
 #define MAX_SOCK_NAME 100
@@ -22,9 +23,24 @@ char* get_sock_name() {
     return sock_path;
 }
 
-int main(int argc, char** argv) {
-    char* sock_name = get_sock_name();
+int get_rand(int lower, int upper) {
+    return (rand() % (upper - lower + 1)) + lower;
+}
 
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("./client client_id nums_to_send");
+    }
+
+    int client_id = atoi(argv[1]);
+    int nums_count = atoi(argv[2]);
+    char* sock_name = get_sock_name();
+    srand(time(0));
+    float sleep_time = get_rand(1, 255) * 1000;
+    if (argc == 4) {
+        sleep_time = atof(argv[3]) * 1000000;
+    }
+    printf("client: %d with delay: %f\n", client_id, sleep_time);
     struct sockaddr_un sockaddr;
     memset(&sockaddr, 0, sizeof(struct sockaddr_un));
     int sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -40,14 +56,22 @@ int main(int argc, char** argv) {
         exit(-1);
     }
 
-    for (int i = 0; i < MAX_READ; i++) {
+    time_t t0 = time(0);
+
+    for (int i = 0; i < nums_count; i++) {
         char send_buf[MAX_READ] = {0};
         char recv_buf[MAX_READ] = {0};
         scanf("%s", send_buf);
+        usleep(sleep_time);
         write(sock_fd, send_buf, strlen(send_buf));
         read(sock_fd, recv_buf, MAX_READ);
         printf("recv: %s\n", recv_buf);
     }
+
+    time_t t1 = time(0);
+
+    double time_in_seconds = difftime(t1, t0);
+    printf("time taken: %f\n", time_in_seconds);
     close(sock_fd);
 
     return 0;
